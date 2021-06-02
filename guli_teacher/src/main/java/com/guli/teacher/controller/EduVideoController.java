85ab3ab48f2160a6aa2534b5e07fe959f44056e8
liujd <liujd@mochasoft.com.cn>
2021-06-02 10:11:24 +0800

    统一返回结果

---
+++ guli_teacher/src/main/java/com/guli/teacher/controller/EduVideoController.java
@@ -0,0 +1,21 @@
+package com.guli.teacher.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 课程视频 前端控制器
+ * </p>
+ *
+ * @author Helen
+ * @since 2021-06-01
+ */
+@RestController
+@RequestMapping("/teacher/edu-video")
+public class EduVideoController {
+
+}
+
---
+++ guli_teacher/src/main/java/com/guli/teacher/mapper/EduChapterMapper.java
@@ -0,0 +1,16 @@
+package com.guli.teacher.mapper;
+
+import com.guli.teacher.entity.EduChapter;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 课程 Mapper 接口
+ * </p>
+ *
+ * @author Helen
+ * @since 2021-06-01
+ */
+public interface EduChapterMapper extends BaseMapper<EduChapter> {
+
+}
