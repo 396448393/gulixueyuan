4f3cabe666a9df6d429744eee62e7b016361665c
liujd <liujd@mochasoft.com.cn>
2022-01-21 15:07:28 +0800

    09-课程列表显示

--- guli_edu/src/main/java/com/guli/edu/controller/admin/CourseAdminController.java
+++ guli_edu/src/main/java/com/guli/edu/controller/admin/CourseAdminController.java
@@ -1,7 +1,10 @@
 package com.guli.edu.controller.admin;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.guli.common.vo.R;
+import com.guli.edu.entity.EduCourse;
 import com.guli.edu.form.CourseInfoForm;
+import com.guli.edu.query.CourseQuery;
 import com.guli.edu.service.EduCourseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModelProperty;
@@ -10,6 +13,9 @@ import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.tags.EditorAwareTag;
+
+import java.util.List;
 
 @Api(description = "课程管理")
 @CrossOrigin //跨域
@@ -41,7 +47,7 @@ public class CourseAdminController {
         CourseInfoForm courseInfoForm = courseService.getCourseInfoFormById(id);
         return R.ok().data("item",courseInfoForm);
     }
-    @ApiModelProperty(value = "更新课程")
+    @ApiOperation(value = "更新课程")
     @PutMapping("update-course-info/{id}")
     public R updateCourseInfoById(
             @ApiParam(name = "CourseInfoForm",value = "课程基本信息",required = true)
@@ -50,5 +56,22 @@ public class CourseAdminController {
             @PathVariable String id){
         courseService.updateCourseInfoById(courseInfoForm);
         return R.ok();
+    }
+    @ApiOperation(value = "分页课程列表")
+    @GetMapping("{page}/{limit}")
+    public R pageQuery(
+            @ApiParam(name = "page", value = "当前页面", required = true)
+            @PathVariable Long page,
+            @ApiParam(name = "limit", value = "每页记录数",required = true)
+            @PathVariable Long limit,
+            @ApiParam(name = "courseQuery",value = "查询对象", required = false)
+            CourseQuery courseQuery
+    ){
+        Page<EduCourse> pageParam = new Page<>(page,limit);
+        courseService.pageQuery(pageParam,courseQuery);
+        List<EduCourse> records = pageParam.getRecords();
+        long total = pageParam.getTotal();
+        return R.ok().data("total",total).data("rows",records);
+
     }
 }
